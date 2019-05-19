08abd02d67657be6816702a6cc12c4af0b5e8b7c
takenoko <takenoko>
2019-05-19 09:53:19 +0900

    complete

--- Chapter02/2-2/03_BestCowLine.py
+++ Chapter02/2-2/03_BestCowLine.py
@@ -5,7 +5,25 @@ S = input()
 a = 0
 b = N -1 
 
+#
 while a <= b:
     #左からみた場合と右から見た場合を比較
     left = False
-    
+    for i in range(0,N):
+        if a + i > b:
+            break
+        if S[a + i] < S[b -i]:
+            left = True
+            break
+        elif S[a+i] > S[b -i]:
+            left = False
+            break
+
+    if left:
+        print(S[a],end='')
+        a += 1
+    else:
+        print(S[b],end='')
+        b -= 1
+print('')
+
